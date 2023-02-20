0a362e7ea87cedc30af0f1878ff9a99df381cc01
Vojtech Ludvik <vojtik1234@gmail.com>
2023-02-20 17:26:46 +0100

    prvni commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+moje nervy
+ */
+using System;
+
+namespace Verzovani1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
